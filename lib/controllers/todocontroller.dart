aa96af1ddce6ed85481b5b674051c45ae5e0b6bf
Danis <05548@smkrus.sch.id>
2025-10-11 18:01:07 +0700

    FEAT [Danis] feat sqlite

--- lib/controllers/todocontroller.dart
+++ lib/controllers/todocontroller.dart
@@ -1,43 +1,52 @@
 import 'package:get/get.dart';
 import '../model/todo.dart';
+import '../db_helper.dart';
 
 class TodoController extends GetxController {
   var todos = <Todo>[].obs;
+  final DBHelper db = DBHelper();
 
-  void addTodo(String title, String desc, String category, DateTime? dueDate) {
-  todos.add(
-    Todo(
-      title: title,
-      description: desc,
-      category: category,
-      dueDate: dueDate,
-    ),
-  );
-}
-
-
-  void markAsDone(int index) {
-    todos[index].isDone = true;
-    todos.refresh();
+  @override
+  void onInit() {
+    super.onInit();
+    loadTodos();
   }
 
-  void editTodo(
-    int index,
-    String newTitle,
-    String newDesc,
-    String newCategory,
-    DateTime? newdueDate,
-) {
-    todos[index].title = newTitle;
-    todos[index].description = newDesc;
-    todos[index].category = newCategory;
-    todos[index].dueDate = newdueDate;
-    todos.refresh();
-}
-
-
-  void deleteTodo(int index) {
-    todos.removeAt(index);
+  Future<void> loadTodos() async {
+    final data = await db.getAll('todos');
+    todos.assignAll(data.map((e) => Todo.fromMap(e)).toList());
   }
-  
+
+  Future<void> addTodo(String title, String desc, String category, DateTime? dueDate) async {
+    final todo = Todo(title: title, description: desc, category: category, dueDate: dueDate);
+    await db.insert('todos', todo.toMap());
+    await loadTodos();
+  }
+
+  Future<void> editTodo(int index, String newTitle, String newDesc, String newCategory, DateTime? newDueDate) async {
+    final todo = todos[index];
+    final updated = Todo(
+      id: todo.id,
+      title: newTitle,
+      description: newDesc,
+      category: newCategory,
+      dueDate: newDueDate,
+      isDone: todo.isDone,
+    );
+    await db.update('todos', updated.toMap(), todo.id!);
+    await loadTodos();
+  }
+
+  Future<void> markAsDone(int index) async {
+    final todo = todos[index];
+    todo.isDone = true;
+    await db.update('todos', todo.toMap(), todo.id!);
+    await loadTodos();
+  }
+
+  Future<void> deleteTodo(int index) async {
+    final todo = todos[index];
+    await db.delete('todos', todo.id!);
+    await loadTodos();
+  }
 }
---
+++ lib/db_helper.dart
@@ -0,0 +1,58 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+  DBHelper._internal();
+
+  static Database? _db;
+
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await _initDb();
+    return _db!;
+  }
+
+  Future<Database> _initDb() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'todo_list.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE todos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            description TEXT,
+            category TEXT,
+            duedate TEXT,
+            isDone INTEGER
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insert(String table, Map<String, dynamic> data) async {
+    final db = await database;
+    return await db.insert(table, data);
+  }
+
+  Future<List<Map<String, dynamic>>> getAll(String table) async {
+    final db = await database;
+    return await db.query(table);
+  }
+
+  Future<int> update(String table, Map<String, dynamic> data, int id) async {
+    final db = await database;
+    return await db.update(table, data, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> delete(String table, int id) async {
+    final db = await database;
+    return await db.delete(table, where: 'id = ?', whereArgs: [id]);
+  }
+}
